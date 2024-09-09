87d259e05ab5fb078185403af55815c652f0a4e9
Andy Pesanweb <andy.yuli@gmail.com>
2024-09-10 04:07:26 +0700

    perubahan tag /update

--- src/reactive.js
+++ src/reactive.js
@@ -12,13 +12,20 @@ class MyReactive extends LitElement{
     this.name = 'Belajar reactive property di LIT';
     this.class = 'Pemula';
  }
+ 
 
  render() {
     return html` <h1>${this.name}</h1>
+    <button @click=${()=>this._changeNameProperty('Rubah nilai "nameeeeee"')}>Ubah teks 
+    </button>
     
     <h3>${this.name}</h3>
     <h6>${this.class}</h6>
     `;
+ }
+
+ _changeNameProperty(value = null) {
+   this.name = value;
  }
 
 }
